fb796ab8c98abed0f58f4e28feb6075687dec9e0
Roberto Segura <rsegura@tiempodevelopment.com>
2018-09-07 17:50:52 -0500

    Basic hardcoded component for the main area

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './components/Sidebar';
+import MainArea from './components/MainArea';
 import './App.scss';
 
 const App = () => {
     return (
-        <Sidebar />
+        <div className='container'>
+            <Sidebar />
+            <MainArea />
+        </div>
     );
 };
 
---
+++ src/components/MainArea.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const MainArea = () => {
+    return (
+        <div className="main-area">
+            <div className="section-info">
+                <div className="subject">
+                    Amazon SES Address Verification Request
+                </div>
+                <div className="actions">
+                    <i className="icon">fiber_manual_record</i>
+                    <i className="icon">delete_outline</i>
+                    <i className="icon">bug_report</i>
+                </div>
+            </div>
+            <div className="section-content">
+                <div className="content">
+                    <div className="section-sender">
+                        <div className="sender">
+                            Amazon.com <a href="">noreply@amazon.com</a>
+                        </div>
+                        <div className="date">
+                            Thursday, May 26, 11:09 AM
+                        </div>
+                    </div>
+                    <div className="body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt. Vitae tempus quam pellentesque nec. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. In hac habitasse platea dictumst quisque sagittis. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Ac turpis egestas integer eget. Molestie nunc non blandit massa enim nec dui nunc mattis. Duis at tellus at urna condimentum mattis pellentesque id nibh. Erat nam at lectus urna duis convallis. Vel pretium lectus quam id leo in. Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Dignissim convallis aenean et tortor.</p>
+
+                        <p>Vitae tempus quam pellentesque nec nam. Diam volutpat commodo sed egestas. Nulla at volutpat diam ut venenatis tellus in. Mus mauris vitae ultricies leo integer malesuada nunc vel risus. Lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Est lorem ipsum dolor sit amet consectetur. Ut etiam sit amet nisl purus in mollis nunc sed. Tellus orci ac auctor augue mauris augue neque gravida in. Tellus in metus vulputate eu scelerisque. Nisi est sit amet facilisis magna etiam. Egestas maecenas pharetra convallis posuere.</p>
+
+                        <p>Massa id neque aliquam vestibulum morbi blandit. Quis varius quam quisque id diam. Nunc scelerisque viverra mauris in aliquam sem fringilla ut. Odio ut enim blandit volutpat maecenas volutpat blandit. Vel eros donec ac odio tempor orci dapibus ultrices in. Felis imperdiet proin fermentum leo vel orci porta. Eu ultrices vitae auctor eu augue ut lectus arcu. Odio euismod lacinia at quis risus sed vulputate odio. Et ultrices neque ornare aenean euismod elementum. Sed felis eget velit aliquet sagittis. Vulputate dignissim suspendisse in est ante in nibh. Mauris pharetra et ultrices neque. Viverra nibh cras pulvinar mattis nunc sed blandit libero. In dictum non consectetur a erat nam at. Et tortor at risus viverra.</p>
+
+                        <p>Sed enim ut sem viverra. Vulputate ut pharetra sit amet aliquam id. Tellus rutrum tellus pellentesque eu tincidunt. Orci a scelerisque purus semper eget duis at tellus. Adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Risus commodo viverra maecenas accumsan. Ac auctor augue mauris augue neque gravida in. Cursus mattis molestie a iaculis at erat. Sit amet consectetur adipiscing elit ut aliquam purus sit. Gravida cum sociis natoque penatibus et. Nibh sed pulvinar proin gravida hendrerit lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada. Amet aliquam id diam maecenas ultricies mi eget mauris.</p>
+
+                        <p>Sed enim ut sem viverra. Vulputate ut pharetra sit amet aliquam id. Tellus rutrum tellus pellentesque eu tincidunt. Orci a scelerisque purus semper eget duis at tellus. Adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Risus commodo viverra maecenas accumsan. Ac auctor augue mauris augue neque gravida in. Cursus mattis molestie a iaculis at erat. Sit amet consectetur adipiscing elit ut aliquam purus sit. Gravida cum sociis natoque penatibus et. Nibh sed pulvinar proin gravida hendrerit lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada. Amet aliquam id diam maecenas ultricies mi eget mauris.</p>
+                        <p>Sed enim ut sem viverra. Vulputate ut pharetra sit amet aliquam id. Tellus rutrum tellus pellentesque eu tincidunt. Orci a scelerisque purus semper eget duis at tellus. Adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Risus commodo viverra maecenas accumsan. Ac auctor augue mauris augue neque gravida in. Cursus mattis molestie a iaculis at erat. Sit amet consectetur adipiscing elit ut aliquam purus sit. Gravida cum sociis natoque penatibus et. Nibh sed pulvinar proin gravida hendrerit lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada. Amet aliquam id diam maecenas ultricies mi eget mauris.</p>
+                        <p>Sed enim ut sem viverra. Vulputate ut pharetra sit amet aliquam id. Tellus rutrum tellus pellentesque eu tincidunt. Orci a scelerisque purus semper eget duis at tellus. Adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Risus commodo viverra maecenas accumsan. Ac auctor augue mauris augue neque gravida in. Cursus mattis molestie a iaculis at erat. Sit amet consectetur adipiscing elit ut aliquam purus sit. Gravida cum sociis natoque penatibus et. Nibh sed pulvinar proin gravida hendrerit lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada. Amet aliquam id diam maecenas ultricies mi eget mauris.</p>
+                        <p>Sed enim ut sem viverra. Vulputate ut pharetra sit amet aliquam id. Tellus rutrum tellus pellentesque eu tincidunt. Orci a scelerisque purus semper eget duis at tellus. Adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Risus commodo viverra maecenas accumsan. Ac auctor augue mauris augue neque gravida in. Cursus mattis molestie a iaculis at erat. Sit amet consectetur adipiscing elit ut aliquam purus sit. Gravida cum sociis natoque penatibus et. Nibh sed pulvinar proin gravida hendrerit lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada. Amet aliquam id diam maecenas ultricies mi eget mauris.</p>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MainArea;
