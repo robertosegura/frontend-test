9f7fe523b736e48a26613f8d2e39896a00ddaba7
Roberto Segura <rsegura@tiempodevelopment.com>
2018-09-07 13:30:10 -0500

    Simple sidebar & using material-icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Sidebar from './components/Sidebar';
+import './App.scss';
 
 const App = () => {
-    return <div>Hello</div>
-}
+    return (
+        <Sidebar />
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-      index: './src/index.js'
+      app: './src/App.js'
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
